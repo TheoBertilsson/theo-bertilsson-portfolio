549c89333c63e59f1f8ac4139158d21235ef4014
Theo Bertilsson <131893433+TheoBertilsson@users.noreply.github.com>
2025-06-04 12:49:01 +0200

    SEO compliant header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,14 +7,15 @@
 </script>
 
 <header class="w-full bg-white">
-	<nav class="flex w-full justify-between px-8 py-7">
-		<a href="/" class=" text-xl font-bold">THEO</a>
+	<nav class="flex w-full justify-between px-8 py-7" aria-label="Main navigation">
+		<a href="/" class="text-xl font-bold">THEO</a>
 		<div class="relative flex size-[30px] w-40 items-center justify-end">
 			{#if isMenuOpen}
 				<ul
 					in:fly={{ duration: 100, x: 30 }}
 					out:fly={{ duration: 100, x: 30 }}
 					class="flex gap-4 pr-12"
+					id="main-menu"
 				>
 					<li class="hover:text-menu-link-hover font-semibold"><a href="/work">Work</a></li>
 					<li class="hover:text-menu-link-hover font-semibold"><a href="/about">About</a></li>
@@ -22,18 +23,24 @@
 				</ul>
 				<button
 					transition:fade={{ duration: 100 }}
-					class="top- absolute flex size-[30px] items-center justify-center p-0.5"
+					class="absolute flex size-[30px] items-center justify-center p-0.5"
 					onclick={() => (isMenuOpen = !isMenuOpen)}
+					aria-label="Close main menu"
+					aria-expanded="true"
+					aria-controls="main-menu"
 				>
-					<img src={closeIcon} alt="Menu" class="size-5" />
+					<img src={closeIcon} alt="Close menu" class="size-5" />
 				</button>
 			{:else}
 				<button
 					transition:fade={{ duration: 100 }}
 					class="absolute flex size-[30px] items-center justify-center p-0.5"
 					onclick={() => (isMenuOpen = !isMenuOpen)}
+					aria-label="Open main menu"
+					aria-expanded="false"
+					aria-controls="main-menu"
 				>
-					<img src={menuIcon} alt="Menu" class="size-5" />
+					<img src={menuIcon} alt="Open menu" class="size-5" />
 				</button>
 			{/if}
 		</div>
